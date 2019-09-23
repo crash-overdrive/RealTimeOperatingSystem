0b71bf7cfc0c10f2db113db650f00b5cef5876ea
spratap <spratap@edu.uwaterloo.ca>
2019-09-23 13:39:56 -0400

    Use RingBuffer to implement PriorityQueues

--- k1/include/PriorityQueue.hpp
+++ k1/include/PriorityQueue.hpp
@@ -1,158 +1,85 @@
 #ifndef PRIORITY_QUEUE_H
     #define PRIORITY_QUEUE_H
     
+    #include "RingBuffer.hpp"
     #include "Util.hpp"
 
     namespace DataStructures {
 
-        // NOTE: Template class T needs to have a default constructor!!
-        // NOTE: Template class T should have a comparator for 2 pointers to evaluate which is greater
-        template <class T, unsigned int length>
+        // NOTE: Template class T needs to have a default constructor!!        
+        // NOTE: Lower priority numbers means high priority
+        template <class T, unsigned int rows, unsigned int columns>
         class PriorityQueue {
+
             protected:
-                const int maxSize = length;
-                int heapSize;
-                T* heap[length];
-                bool (* isGreaterThan)(T*, T*);
 
-                // to get index of parent of node at index "index"
-                int parentIndex(int index) { 
-                    
-                    return (index-1)/2; 
-                    
-                } 
-            
-                // to get index of left child of node at index "index"
-                int leftChildIndex(int index) { 
-
-                    return (2*index + 1); 
-                    
-                } 
-            
-                // to get index of right child of node at index "index"
-                int rightChildIndex(int index) { 
-                    
-                    return (2*index + 2); 
-                    
-                } 
-
-                // to heapify a subtree with the root at given index 
-                void MaxHeapify(int currentIndex) {
-
-                    int leftIndex = leftChildIndex(currentIndex); 
-                    int rightIndex = rightChildIndex(currentIndex); 
-                    int largestElementIndex = currentIndex; 
-                    
-                    if (leftIndex < heapSize && isGreaterThan(heap[leftIndex], heap[largestElementIndex])) {
-
-                        largestElementIndex = leftIndex; 
-
-                    }
-                        
-                    if (rightIndex < heapSize && isGreaterThan(heap[rightIndex], heap[largestElementIndex])) {
-
-                        largestElementIndex = rightIndex;
-
-                    } 
-                         
-                    if (largestElementIndex != currentIndex) { 
-                        
-                        Util::swap(&heap[currentIndex], &heap[largestElementIndex]); 
-                        MaxHeapify(largestElementIndex); 
-
-                    } 
-
-                }
-                
+                const int maxNumberOfPriorities = rows;
+                const int maxNumberOfSamePriorities = columns;
+                RingBuffer<T, columns> queue[rows];
 
             public:
-                PriorityQueue<T, length>(bool (* comparator)(T*, T*)) {
 
-                    heapSize = 0;
-                    isGreaterThan = comparator;
-
-                };
-
-                // Inserts a new key 'key' 
-                // Does nothing if queue is already full
-                void push(T* key) {
-                    
-                    if (heapSize == maxSize) { 
-
-                        return; 
-
-                    } 
-                
-                    // First insert the new key at the end of heap
-                    heapSize++; 
-                    int indexToBeInsertedAt = heapSize - 1; 
-                    heap[indexToBeInsertedAt] = key; 
-                
-                    // Fix the max heap property if it is violated 
-                    while (indexToBeInsertedAt != 0 && isGreaterThan(heap[indexToBeInsertedAt], heap[parentIndex(indexToBeInsertedAt)])) {
-
-                        Util::swap(&heap[indexToBeInsertedAt], &heap[parentIndex(indexToBeInsertedAt)]); 
-                        indexToBeInsertedAt = parentIndex(indexToBeInsertedAt); 
-
-                    } 
+                PriorityQueue<T, rows, columns>() {
 
                 }
-            
-                // Returns the maximum key (key at root) from max heap (and removes it from queue)
-                // Returns nullptr if called on empty queue
-                T* pop() { 
-                    
-                    if (heapSize <= 0) {
 
-                        return nullptr;
+                // NOTE: if ring buffer of priority is full, then put will overwrite the oldest value
+                // NOTE: if priority is not in the range then its ignored
+                void push(T* item, int priority) {
+                    
+                    if (priority >= 0 && priority < maxNumberOfPriorities) {
+
+                        queue[priority].push(item);
 
                     }
- 
-                    if (heapSize == 1) { 
 
-                        heapSize--; 
-                        return heap[0]; 
+                }
 
-                    } 
-                
-                    // Store the maximum value, and remove it from heap 
-                    T* root = heap[0]; 
-                    heap[0] = heap[heapSize-1]; 
-                    heapSize--; 
-                    MaxHeapify(0); 
-                
-                    return root; 
-                    
-                } 
+                // NOTE: if all RingBuffers were empty then will return nullptr
+                T* pop() {
+
+                    for(int i = 0; i < maxNumberOfPriorities; ++i) {
+
+                        if (!(queue[i].empty())) {
+
+                            T* val = queue[i].pop();
+                            return val;
+
+                        }
+
+                    }
+
+                    return nullptr;
+
+                }
 
                 void reset() {
+                    
+                    for(int i = 0; i < maxNumberOfPriorities; ++i) {
 
-                    heapSize = 0;
+                        queue[i].reset();
+
+                    }
+                    
 
                 }
 
                 bool empty() const {
 
-                    //if head and tail are equal and RingBuffer is not full, then RingBuffer is empty
-                    return (heapSize <= 0);
+                    bool isEmpty = true;
 
-                }
+                    for(int i = 0; i < maxNumberOfPriorities; ++i) {
 
-                bool full() const {
+                        if (!(queue[i].empty())) {
 
-                    return (heapSize >= maxSize);
+                            isEmpty = false;
+                            break;
 
-                }
+                        }
 
-                unsigned int capacity() const {
-                    
-                    return maxSize;
-                
-                }
+                    }
 
-                unsigned int size() const {
-
-                    return heapSize;
+                    return isEmpty;
 
                 }
 
--- k1/test/TestPriorityQueue.hpp
+++ k1/test/TestPriorityQueue.hpp
@@ -2,39 +2,21 @@
 #include "../include/Util.hpp" 
 
 struct TestClass {
+
     int priority;
+    char* info;
 
     TestClass() {
         priority = -1;
+        info = nullptr;
     }
 
-    TestClass (int p) {
+    TestClass (int p, char* c) {
         priority = p;
-    }
-
-    static bool comparator(TestClass* test1, TestClass* test2) {
-        if (test1->priority > test2->priority) {
-            return true;
-        }
-        return false;
+        info = c;
     }
 
 };
-
-
-bool isGreaterThanForChar(char* c1, char* c2) {
-    if(*c1 > *c2) {
-        return true;
-    }
-    return false;
-}
-
-bool isGreaterThanForInt(int* c1, int* c2) {
-    if(*c1 > *c2) {
-        return true;
-    }
-    return false;
-}
 
 class TestPriorityQueue {
     public:
@@ -47,139 +29,146 @@ class TestPriorityQueue {
             char c5 = 'k';
             char c6 = 'p';
             char c7 = 'w';
-
-            TestClass td1 = TestClass(100);
-            TestClass td2 = TestClass(300);
-            TestClass td3 = TestClass(500);
-            TestClass td4 = TestClass(10);
-            TestClass td5 = TestClass(5);
-            TestClass td6 = TestClass(1000);
-            TestClass td7 = TestClass(7000);
             
-            DataStructures::PriorityQueue<char, 5> pqueueChar(isGreaterThanForChar);
+            DataStructures::PriorityQueue<char, 4, 2> pqueueChar;
 
-            pqueueChar.push(&c1);
-            pqueueChar.push(&c2);
-            pqueueChar.push(&c3);
-            pqueueChar.push(&c4);
-            pqueueChar.push(&c5);
+            pqueueChar.push(&c1, 1);
+            pqueueChar.push(&c2, 2);
+            pqueueChar.push(&c3, 3);
+            pqueueChar.push(&c4, 1);
+            pqueueChar.push(&c5, 2);
             pqueueChar.reset();
 
             Util::assert(pqueueChar.empty() == true);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 0);
+            Util::assert(pqueueChar.pop() == nullptr);
             
-            pqueueChar.push(&c1);
-            pqueueChar.push(&c2);
-            pqueueChar.push(&c3);
-            pqueueChar.push(&c4);
-            pqueueChar.push(&c5);
+            pqueueChar.push(&c1, 1);
+            pqueueChar.push(&c2, 2);
+            pqueueChar.push(&c3, 3);
+            pqueueChar.push(&c4, 1);
+            pqueueChar.push(&c5, 2);
 
             Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == true);
-            Util::assert(pqueueChar.size() == 5);
-            Util::assert(*pqueueChar.pop() == 'z');
-
-            Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 4);
-            Util::assert(*pqueueChar.pop() == 'x');
-
-            pqueueChar.push(&c6);
-            pqueueChar.push(&c7);
-
-            Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == true);
-            Util::assert(pqueueChar.size() == 5);
-            Util::assert(*pqueueChar.pop() == 'w');
-
-            Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 4);
-            Util::assert(*pqueueChar.pop() == 'p');
-
-            Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 3);
-            Util::assert(*pqueueChar.pop() == 'k');
-
-            Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 2);
             Util::assert(*pqueueChar.pop() == 'g');
 
             Util::assert(pqueueChar.empty() == false);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 1);
+            Util::assert(*pqueueChar.pop() == 'z');
+
+            pqueueChar.push(&c6, 1);
+            pqueueChar.push(&c7, 1);
+
+            Util::assert(pqueueChar.empty() == false);
+            Util::assert(*pqueueChar.pop() == 'p');
+
+            Util::assert(pqueueChar.empty() == false);
+            Util::assert(*pqueueChar.pop() == 'w');
+
+            Util::assert(pqueueChar.empty() == false);
             Util::assert(*pqueueChar.pop() == 'a');
 
+            Util::assert(pqueueChar.empty() == false);
+            Util::assert(*pqueueChar.pop() == 'k');
+
+            Util::assert(pqueueChar.empty() == false);
+            Util::assert(*pqueueChar.pop() == 'x');
+
             Util::assert(pqueueChar.empty() == true);
-            Util::assert(pqueueChar.full() == false);
-            Util::assert(pqueueChar.size() == 0);
+            Util::assert(pqueueChar.pop() == nullptr);
 
+            
+            TestClass td1 = TestClass(100, "first");
+            TestClass td2 = TestClass(300, "second");
+            TestClass td3 = TestClass(500, "third");
+            TestClass td4 = TestClass(10, "fourth");
+            TestClass td5 = TestClass(5, "fifth");
+            TestClass td6 = TestClass(1000, "sixth");
+            TestClass td7 = TestClass(7000, "seventh");
+            TestClass td8 = TestClass(100, "eight");
+            TestClass td9 = TestClass(100, "ninth");
 
-            DataStructures::PriorityQueue<TestClass, 5> pqueue(TestClass::comparator);
+            DataStructures::PriorityQueue<TestClass, 4, 3> pqueue;
 
-            Util::assert(pqueue.capacity() == 5);
-
-            pqueue.push(&td1);
-            pqueue.push(&td2);
-            pqueue.push(&td3);
-            pqueue.push(&td4);
-            pqueue.push(&td5);
+            pqueue.push(&td1, 0);
+            pqueue.push(&td2, 1);
+            pqueue.push(&td3, 2);
+            pqueue.push(&td4, 3);
+            pqueue.push(&td5, 0);
             pqueue.reset();
 
             Util::assert(pqueue.empty() == true);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 0);
+            Util::assert(pqueueChar.pop() == nullptr);
             
-            pqueue.push(&td1);
-            pqueue.push(&td2);
-            pqueue.push(&td3);
-            pqueue.push(&td4);
-            pqueue.push(&td5);
+            pqueue.push(&td1, 0);
+            pqueue.push(&td2, 1);
+            pqueue.push(&td3, 2);
+            pqueue.push(&td4, 3);
+            pqueue.push(&td5, 0);
+            pqueue.push(&td6, 1);
+            pqueue.push(&td7, 2);
+            pqueue.push(&td8, 3);
+            pqueue.push(&td9, 0);
+            pqueue.push(&td1, 1);
+            pqueue.push(&td2, 2);
+            pqueue.push(&td3, 3);
+
+            TestClass* temp;
 
             Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == true);
-            Util::assert(pqueue.size() == 5);
-            Util::assert(pqueue.pop()->priority == 500);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td1.priority && temp->info == td1.info);
 
             Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 4);
-            Util::assert(pqueue.pop()->priority == 300);
-
-            pqueue.push(&td7);
-            pqueue.push(&td6);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td5.priority && temp->info == td5.info);
 
             Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == true);
-            Util::assert(pqueue.size() == 5);
-            Util::assert(pqueue.pop()->priority == 7000);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td9.priority && temp->info == td9.info);
 
             Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 4);
-            Util::assert(pqueue.pop()->priority == 1000);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td2.priority && temp->info == td2.info);
 
             Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 3);
-            Util::assert(pqueue.pop()->priority == 100);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td6.priority && temp->info == td6.info);
 
-            Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 2);
-            Util::assert(pqueue.pop()->priority == 10);
+            pqueue.push(&td4, 0);
             
             Util::assert(pqueue.empty() == false);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 1);
-            Util::assert(pqueue.pop()->priority == 5);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td4.priority && temp->info == td4.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td1.priority && temp->info == td1.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td3.priority && temp->info == td3.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td7.priority && temp->info == td7.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td2.priority && temp->info == td2.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td4.priority && temp->info == td4.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td8.priority && temp->info == td8.info);
+
+            Util::assert(pqueue.empty() == false);
+            temp = pqueue.pop();
+            Util::assert(temp->priority == td3.priority && temp->info == td3.info);
 
             Util::assert(pqueue.empty() == true);
-            Util::assert(pqueue.full() == false);
-            Util::assert(pqueue.size() == 0);
+            Util::assert(pqueueChar.pop() == nullptr);
 
-        }
+    }
 };
