d89ca01786dc11ee3e87e37858fccedf046f3004
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-09-24 13:15:52 -0400

    Create initial program loop

--- k1/src/main/main.cpp
+++ k1/src/main/main.cpp
@@ -1,16 +1,21 @@
 #define FOREVER for (;;)
+#include "../kern/Kernel.cpp"
 
 extern "C" {
 	#include <bwio.h>
 }
 
 int main () {
-    // initialize(); // includes starting the first task
-    // FOREVER {
-    //     active = schedule();
-    //     request = activate(active);
-    //     handle(request)
-    // }
+    Kernel kern;
+    TaskDescriptor *active;
+    int request;
+
+    kern.initialize(); // includes starting the first task
+    FOREVER {
+        active = kern.schedule();
+        request = kern.activate(active);
+        kern.handle(request);
+    }
 	bwprintf(COM2, "It works!");
 
 }
