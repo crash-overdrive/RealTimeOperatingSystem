da9430f4d4124b743046f81865b5b0beb9de8ff8
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-09-24 13:14:32 -0400

    Add more constant definitions

--- k1/include/Constants.hpp
+++ k1/include/Constants.hpp
@@ -4,7 +4,9 @@
 namespace Constants {
     const int NUM_PRIORITIES = 8;
     const int Q_LENGTH = 4;
-    const int KIBIBYTE = 262144;
+    const int EXIT_Q_LENGTH = 128;
+    const int TD_STACK_SIZE = 131072;
+    const int NUM_TASKS = 128;
     const enum STATE {
         ACTIVE,
         READY,
