529aec4eac3a2e42dadadc714c4c24aaad7f7f41
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-09-24 13:15:07 -0400

    Setup initial kernel workflow

--- k1/src/kern/Kernel.cpp
+++ k1/src/kern/Kernel.cpp
@@ -7,12 +7,28 @@ class Kernel {
     private:
         DataStructures::PriorityQueue<TaskDescriptor, Constants::NUM_PRIORITIES, Constants::Q_LENGTH> ready_queue;
         DataStructures::RingBuffer<TaskDescriptor, Constants::Q_LENGTH> blocked_queue;
-        DataStructures::RingBuffer<TaskDescriptor, Constants::Q_LENGTH> next_queue;
+        DataStructures::RingBuffer<TaskDescriptor, Constants::EXIT_Q_LENGTH> exit_queue;
+
+        TaskDescriptor tasks[Constants::NUM_TASKS];
 
     public:
+        void initialize() {
+            // TODO: implement me
+            // setup comm
+        }
+
         // Returns the task descriptor of the next scheduled task
         TaskDescriptor *schedule() {
             return ready_queue.pop();
+        }
+
+        int activate(TaskDescriptor *active) {
+            // TODO: implement me
+        }
+
+        // TODO: determine if this should return anything
+        void handle(int request) {
+            // TODO: implement me
         }
 
 };
