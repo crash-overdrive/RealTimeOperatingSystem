e23a93260ffb6bb73901d8198a068bd308328602
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-09-25 17:12:57 -0400

    Fix whitespace

--- k1/src/kern/Syscall.cpp
+++ k1/src/kern/Syscall.cpp
@@ -13,26 +13,21 @@ int Create(int priority, void (*function)()) {
     return sysCreate(priority, function);
 }
 
-
 int MyTid() {
     return sysMyTid();
 }
-
 
 int MyParentTid() {
     return sysMyParentTid();
 }
 
-
 void Yield() {
     sysYield();
 }
 
-
 void Exit() {
     sysExit();
 }
-
 
 int sysCreate(int priority, void (*function)()) {
     // r0 = priority, r1 = function ptr
@@ -41,13 +36,11 @@ int sysCreate(int priority, void (*function)()) {
     asm volatile("ldmai {r0-r3, r12}");
 }
 
-
 int sysMyTid() {
     asm volatile("stmdb {r0-r3, r12}");
     asm volatile("swi 3");
     asm volatile("ldmai {r0-r3, r12}");
 }
-
 
 int sysMyParentTid() {
     asm volatile("stmdb {r0-r3, r12}");
@@ -55,13 +48,11 @@ int sysMyParentTid() {
     asm volatile("ldmai {r0-r3, r12}");
 }
 
-
 void sysYield() {
     asm volatile("stmdb {r0-r3, r12}");
     asm volatile("swi 5");
     asm volatile("ldmai {r0-r3, r12}");
 }
-
 
 void sysExit() {
     asm volatile("stmdb {r0-r3, r12}");
