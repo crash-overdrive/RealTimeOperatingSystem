098b685d11db1399124df978e7ad2d698085c517
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-10-18 20:31:07 -0400

    Replicate uart2rx logic to uart1rx

--- k4/src/user/server/UART1RXServer.cpp
+++ k4/src/user/server/UART1RXServer.cpp
@@ -1,6 +1,8 @@
 #include "Constants.hpp"
 #include "data-structures/RingBuffer.hpp"
 #include "io/bwio.hpp"
+#include "io/ts7200.h"
+#include "io/UART.hpp"
 #include "user/client/UART1RXNotifier.hpp"
 #include "user/server/UART1RXServer.hpp"
 #include "user/syscall/UserSyscall.hpp"
@@ -8,32 +10,47 @@
 #define FOREVER for (;;)
 
 void uart1rxServer() {
-    // bwprintf(COM2, "U2C\n\r");
+    // bwprintf(COM2, "UART1RX Server - entered server\n\r");
+
     // Server variables
     int tid;
     char msg[Constants::UART1RXServer::MSG_SIZE];
     int msglen;
     char reply[Constants::UART1RXServer::RP_SIZE];
+    UART uart1 = UART(UART1_BASE);
+    DataStructures::RingBuffer<char, Constants::UART1RXServer::BUFFER_SIZE> rbuf;
+    DataStructures::RingBuffer<int, Constants::UART1RXServer::BUFFER_SIZE> waitbuf;
+
+    RegisterAs("UART1RX");
 
     int notifierTid = Create(0, uart1rxNotifier);
+    // bwprintf(COM2, "UART1RX Server - created notifier with tid %d\n\r", notifierTid);
 
-    DataStructures::RingBuffer<char, Constants::UART1RXServer::BUFFER_SIZE> rbuf;
-
-    RegisterAs("UART1RX SERVER");
 
     FOREVER {
         msglen = Receive(&tid, msg, Constants::UART1RXServer::MSG_SIZE);
+
         if (tid == notifierTid) {
-            // Get the data and put it in the ringbuffer
-            for (int i = 0; i < msglen; ++i) {
-                // rbuf.push(msg[i]);
+            while (!uart1.isRXEmpty()) {
+                rbuf.push(uart1.getc());
             }
+
             reply[0] = Constants::Server::ACK;
             Reply(tid, reply, 1);
+            if (!waitbuf.empty()) {
+                tid = waitbuf.pop();
+                reply[0] = rbuf.pop();
+                Reply(tid, reply, 1);
+            }
+            uart1.enableRXInterrupt();
         } else {
             // Request is coming from the kernel, so return a character from the read buffer
-            // reply[0] = rbuf.pop();
-            Reply(tid, reply, 1);
+            if (rbuf.empty()) {
+                waitbuf.push(tid);
+            } else {
+                reply[0] = rbuf.pop();
+                Reply(tid, reply, 1);
+            }
         }
     }
 }
