76cbaba3cb69823bb8ec2c132088ad4140394c02
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-10-18 20:09:47 -0400

    Modify uart2rx to block tasks when no character available

--- k4/src/user/server/UART2RXServer.cpp
+++ k4/src/user/server/UART2RXServer.cpp
@@ -11,21 +11,21 @@
 
 void uart2rxServer() {
     // bwprintf(COM2, "UART2RX Server - entered server\n\r");
-    // bwprintf(COM2, "U2C\n\r");
+
     // Server variables
     int tid;
     char msg[Constants::UART2RXServer::MSG_SIZE];
     int msglen;
     char reply[Constants::UART2RXServer::RP_SIZE];
     UART uart2 = UART(UART2_BASE);
-    // TODO(sgaweda): Add queue of tasks waiting for characters
+    DataStructures::RingBuffer<char, Constants::UART2RXServer::BUFFER_SIZE> rbuf;
+    DataStructures::RingBuffer<int, Constants::UART2RXServer::BUFFER_SIZE> waitbuf;
 
     RegisterAs("UART2RX");
 
     int notifierTid = Create(0, uart2rxNotifier);
     // bwprintf(COM2, "UART2RX Server - created notifier with tid %d\n\r", notifierTid);
 
-    DataStructures::RingBuffer<char, Constants::UART2RXServer::BUFFER_SIZE> rbuf;
 
     FOREVER {
         msglen = Receive(&tid, msg, Constants::UART2RXServer::MSG_SIZE);
@@ -37,13 +37,19 @@ void uart2rxServer() {
 
             reply[0] = Constants::Server::ACK;
             Reply(tid, reply, 1);
+            if (!waitbuf.empty()) {
+                tid = waitbuf.pop();
+                reply[0] = rbuf.pop();
+                Reply(tid, reply, 1);
+            }
             uart2.enableRXInterrupt();
             // bwprintf(COM2, "UART2RX Server - re-enabling interrupts\n\r");
         } else {
             // Request is coming from the kernel, so return a character from the read buffer
             if (rbuf.empty()) {
-                reply[0] = 0; // If you get 0 just try again Shash, in order for this to be properly blocking, we need a queue of people to repond to and I'm not sure we need it
-                Reply(tid, reply, 1);
+                waitbuf.push(tid);
+                // reply[0] = 0; // If you get 0 just try again Shash, in order for this to be properly blocking, we need a queue of people to repond to and I'm not sure we need it
+                // Reply(tid, reply, 1);
             } else {
                 reply[0] = rbuf.pop();
                 Reply(tid, reply, 1);
