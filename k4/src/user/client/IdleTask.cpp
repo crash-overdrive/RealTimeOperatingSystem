09d78fbed53fa143ec756382ec2e85f0f0eef44f
spratap <spratap@edu.uwaterloo.ca>
2019-11-06 16:40:16 -0500

    Final idleTask changes

--- k4/src/user/client/IdleTask.cpp
+++ k4/src/user/client/IdleTask.cpp
@@ -15,13 +15,22 @@ void idleTask() {
     *(int *)(TIMER2_BASE + LDR_OFFSET) = 0xFFFFFFFF;
     *(int *)(TIMER2_BASE + CRTL_OFFSET) = ENABLE_MASK | MODE_MASK | CLKSEL_MASK;
     int UART2_TX = WhoIs("UART2TX");
-
     FOREVER {
-        int idleTime = Halt();
+        int timeSpentInIdleFor50Ticks = 0;
+        int idleTimePrevious = 0;
+        int i = 1;
+        for (; i <= 50; ) {
+            int idleTime = Halt();
+            if (idleTime != idleTimePrevious) {
+                ++i;
+                idleTimePrevious = idleTime;
+                timeSpentInIdleFor50Ticks += idleTime;
+            }
+        }
         char buffer[50];
-        int length = format(buffer, "\033[s\033[0;10H%d.%d%%\033[u", idleTime * 10 / 508 , idleTime * 10 % 508);
-        for (int i = 0; i < length; ++i) {
-            Putc(UART2_TX, 1, buffer[i]);
+        int length = format(buffer, "\033[s\033[0;10H%d.%d%%\033[u", timeSpentInIdleFor50Ticks * 10 / i / 508 , timeSpentInIdleFor50Ticks * 10 / i % 508);
+        for (int j = 0; j < length; ++j) {
+            Putc(UART2_TX, 1, buffer[j]);
         }
     }
 }
