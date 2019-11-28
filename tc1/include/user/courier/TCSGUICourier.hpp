8114b31011825eb7b5adfc09f0d40ede2a548688
Sebastian <sgaweda@edu.uwaterloo.ca>
2019-11-28 12:36:35 -0500

    Remove obsolete TCS files

--- tc1/include/user/courier/TCSGUICourier.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TCS_GUI_COURIER_HPP
-#define TCS_GUI_COURIER_HPP
-
-void tcsGUICourier();
-
-#endif
--- tc1/include/user/courier/TermTCSCourier.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TERM_TCS_COURIER_HPP
-#define TERM_TCS_COURIER_HPP
-
-void termTCSCourier();
-
-#endif
